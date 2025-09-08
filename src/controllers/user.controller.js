67158bf05f016db49715749c4f2e51a1f12c3325
Amit <amiiitk16@gmail.com>
2025-09-08 20:57:41 +0530

    fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -278,7 +278,7 @@ const getCurrentUser = asyncHandler(async(req, res) =>
 {
     return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully")
+    .json(new ApiResponse( 200, req.user, "current user fetched successfully"))
 })
 
 
@@ -352,7 +352,7 @@ export {
     changeCurrentUserPassword,
     updateAccountDetails,
     updateUserAvatar,
-    
+
 
 }
 
